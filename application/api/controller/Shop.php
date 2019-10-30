6a5a8425567d9ea77f8bbab7cc00307552667e5a
sleepcat1210 <18224513696@163.com>
2019-10-30 17:01:42 +0800

    添加商户申请

--- application/api/controller/Shop.php
+++ application/api/controller/Shop.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace app\api\controller;
+
 use app\api\model\Comment;
 use app\api\model\Report;
 use app\api\model\Shop as ShopModel;
@@ -11,7 +13,7 @@ class Shop extends Base
     public function index()
     {
         $shop_id = input('post.shop_id');
-        if(null===$shop_id){
+        if (null === $shop_id) {
             $this->json_error('请传过来商家编号');
         }
 
@@ -20,37 +22,37 @@ class Shop extends Base
 
 
 
-        $res = Db::name('comment')->where(['shop_id'=>$shop_id])->field('sum(logistics) as logistics,sum(manner) as manner')->group('shop_id')->select();
+        $res = Db::name('comment')->where(['shop_id' => $shop_id])->field('sum(logistics) as logistics,sum(manner) as manner')->group('shop_id')->select();
 
 
 
-        $num = Db::name('comment')->where(['shop_id'=>$shop_id])->count();
+        $num = Db::name('comment')->where(['shop_id' => $shop_id])->count();
 
 
         //物流服务  logistics
         //服务态度  manner
         //liu
-        $shop = Db::name("shop")->where('id',$shop_id)->field('id,name,shoplogo,intro,province,city,area,address,description,quality,service')->find();
-        if(empty($shop)){
+        $shop = Db::name("shop")->where('id', $shop_id)->field('id,name,shoplogo,intro,province,city,area,address,description,quality,service')->find();
+        if (empty($shop)) {
             $this->json_error('商家不存在！');
         }
-        $shop['shop_fans']=ShopModel::get($shop_id)->shopFans()->count();
-        $shop['shop_goods_num']=ShopModel::get($shop_id)->shopGoodsNum()->count();
-        $shop['shoplogo'] = $this->domain().$shop['shoplogo'];       
-        $shop['sale_num']=ShopModel::shopOrderNum($shop_id);
-        if($res!=null){
+        $shop['shop_fans'] = ShopModel::get($shop_id)->shopFans()->count();
+        $shop['shop_goods_num'] = ShopModel::get($shop_id)->shopGoodsNum()->count();
+        $shop['shoplogo'] = $this->domain() . $shop['shoplogo'];
+        $shop['sale_num'] = ShopModel::shopOrderNum($shop_id);
+        if ($res != null) {
             $res = $res[0];
-            if($num>0){
-                $shop['scomment'] = $num/$tnum;
-                $shop['logistics'] = $res['logistics'] /$num;
-                $shop['manner'] = $res['manner'] /$num;
-            }else{
-                $shop['scomment'] = $num/$tnum;
+            if ($num > 0) {
+                $shop['scomment'] = $num / $tnum;
+                $shop['logistics'] = $res['logistics'] / $num;
+                $shop['manner'] = $res['manner'] / $num;
+            } else {
+                $shop['scomment'] = $num / $tnum;
                 $shop['logistics'] = 0;
                 $shop['manner'] = 0;
             }
-        }else{
-            $shop['scomment'] = $num/$tnum;
+        } else {
+            $shop['scomment'] = $num / $tnum;
             $shop['logistics'] = 0;
             $shop['manner'] = 0;
         }
@@ -58,22 +60,22 @@ class Shop extends Base
 
 
         $user_id = input('post.user_id');
-        if(null!=$user_id){
+        if (null != $user_id) {
             //登录了
-            $collectionshop = db('collectionshop')->where(['user_id'=>$user_id,'shop_id'=>$shop_id])->find();
-            if(null!=$collectionshop){
+            $collectionshop = db('collectionshop')->where(['user_id' => $user_id, 'shop_id' => $shop_id])->find();
+            if (null != $collectionshop) {
                 //代表收藏了
                 $shop['is_collectionshop'] = 1;
-            }else{
+            } else {
                 //没有收藏
                 $shop['is_collectionshop'] = 0;
             }
-        }else{
+        } else {
             //没有登录
             $shop['is_collectionshop'] = 0;
         }
         //收藏商家数量
-        $collectionshop = Db::name('collectionshop')->where('shop_id',$shop_id)->count();
+        $collectionshop = Db::name('collectionshop')->where('shop_id', $shop_id)->count();
         $shop['collectionshop'] = $collectionshop;
 
         $data = [];
@@ -84,55 +86,55 @@ class Shop extends Base
         //筛选type   1 最新    2全部   3高价格    4低价格
 
         //每页显示的数量
-        $type = empty(input('post.type'))?1:input('post.type');
+        $type = empty(input('post.type')) ? 1 : input('post.type');
 
         //当前的页码
-        $p = empty(input('post.p')) ?1:input('post.p');
+        $p = empty(input('post.p')) ? 1 : input('post.p');
 
 
         //每页显示的数量
-        $rows = empty(input('post.rows'))?30:input('post.rows');
+        $rows = empty(input('post.rows')) ? 30 : input('post.rows');
 
 
-        switch($type){
+        switch ($type) {
             case 1:
                 //1 最新
-                $goods = \app\api\model\Goods::where('shopid',$shop_id)
+                $goods = \app\api\model\Goods::where('shopid', $shop_id)
                     ->field('id,title,headimg,price')
-                    ->order('id','desc')
-                    ->page($p,$rows)
+                    ->order('id', 'desc')
+                    ->page($p, $rows)
                     ->select();
                 break;
             case 2:
                 //2全部
-                $goods = \app\api\model\Goods::where('shopid',$shop_id)
+                $goods = \app\api\model\Goods::where('shopid', $shop_id)
                     ->field('id,title,headimg,price')
-                    ->order('id','desc')
-                    ->page($p,$rows)
+                    ->order('id', 'desc')
+                    ->page($p, $rows)
                     ->select();
                 break;
             case 3:
                 //3高价格
-                $goods = \app\api\model\Goods::where('shopid',$shop_id)
+                $goods = \app\api\model\Goods::where('shopid', $shop_id)
                     ->field('id,title,headimg,price')
-                    ->order('price','desc')
-                    ->page($p,$rows)
+                    ->order('price', 'desc')
+                    ->page($p, $rows)
                     ->select();
                 break;
             case 4:
                 //4低价格
-                $goods = \app\api\model\Goods::where('shopid',$shop_id)
+                $goods = \app\api\model\Goods::where('shopid', $shop_id)
                     ->field('id,title,headimg,price')
-                    ->order('price','asc')
-                    ->page($p,$rows)
+                    ->order('price', 'asc')
+                    ->page($p, $rows)
                     ->select();
                 break;
             default:
                 //1 最新
-                $goods = \app\api\model\Goods::where('shopid',$shop_id)
+                $goods = \app\api\model\Goods::where('shopid', $shop_id)
                     ->field('id,title,headimg,price')
-                    ->order('id','desc')
-                    ->page($p,$rows)
+                    ->order('id', 'desc')
+                    ->page($p, $rows)
                     ->select();
                 break;
         }
@@ -140,22 +142,22 @@ class Shop extends Base
 
 
         $myimgs = [];
-        foreach($goods as $k=>$v){
-            $headimg = explode(',',$v['headimg']);
-            $goods[$k]['headimg'] = $this->domain().$headimg[0];
-            $collectiongoods = Db::name('collectiongoods')->where('goods_id',$v['id'])->count();
+        foreach ($goods as $k => $v) {
+            $headimg = explode(',', $v['headimg']);
+            $goods[$k]['headimg'] = $this->domain() . $headimg[0];
+            $collectiongoods = Db::name('collectiongoods')->where('goods_id', $v['id'])->count();
             $goods[$k]['collectiongoods'] = $collectiongoods;
-            if(null!=$user_id){
+            if (null != $user_id) {
                 //登录了
-                $collectiongoods = db('collectiongoods')->where(['user_id'=>$user_id,'goods_id'=>$v['id']])->find();
-                if(null!=$collectiongoods){
+                $collectiongoods = db('collectiongoods')->where(['user_id' => $user_id, 'goods_id' => $v['id']])->find();
+                if (null != $collectiongoods) {
                     //代表收藏了
                     $goods[$k]['is_collectiongoods'] = 1;
-                }else{
+                } else {
                     //没有收藏
                     $goods[$k]['is_collectiongoods'] = 0;
                 }
-            }else{
+            } else {
                 //没有登录
                 $goods[$k]['is_collectiongoods'] = 0;
             }
@@ -171,118 +173,71 @@ class Shop extends Base
     public function report()
     {
         $shop_id = input('post.shop_id');
-        if(null===$shop_id){
+        if (null === $shop_id) {
             $this->json_error('请传过来商家编号');
         }
-        $rule=[
-            'reason'=> 'require',
-            'describe'=>'require'
+        $rule = [
+            'reason' => 'require',
+            'describe' => 'require'
         ];
-        $msg=[
-            'reason.require'=>'举报原因不能为空',
-            'describe.require'=>'举报描述不能为空'
+        $msg = [
+            'reason.require' => '举报原因不能为空',
+            'describe.require' => '举报描述不能为空'
         ];
-        $result=$this->validate(input('post.'),$rule,$msg);
-        if(true !== $result){
+        $result = $this->validate(input('post.'), $rule, $msg);
+        if (true !== $result) {
             // 验证失败 输出错误信息
             $this->json_error($result);
             die;
-        }else{
-            if(!empty(input('post.imgsrc'))){
+        } else {
+            if (!empty(input('post.imgsrc'))) {
                 $data = [
-                    'shop_id'=>$shop_id,
-                    'reason'=>input('post.reason'),
-                    'describe'=>input('post.describe'),
-                    'imgsrc'=>input('post.imgsrc'),
-                    'add_time'=>time()
+                    'shop_id' => $shop_id,
+                    'reason' => input('post.reason'),
+                    'describe' => input('post.describe'),
+                    'imgsrc' => input('post.imgsrc'),
+                    'add_time' => time()
                 ];
-
-            }else{
+            } else {
                 $data = [
-                    'shop_id'=>$shop_id,
-                    'reason'=>input('post.reason'),
-                    'describe'=>input('post.describe'),
-                    'add_time'=>time()
+                    'shop_id' => $shop_id,
+                    'reason' => input('post.reason'),
+                    'describe' => input('post.describe'),
+                    'add_time' => time()
                 ];
             }
 
             $id = Report::insertGetId($data);
-            if($id){
+            if ($id) {
                 $info = Report::find($id);
-                $this->json_success($info,'举报成功');
-            }else{
+                $this->json_success($info, '举报成功');
+            } else {
                 $this->json_error('举报失败');
             }
-
         }
-
     }
     //商家信息
-    public function shopinfos(){
-        $shop_id=input("post.shop_id");
-        $shopInfo=Db::name("shop")
-                        ->field("shoplogo,name,intro,content,longitude,latitude,addtime,star,province,city,area,street,quality,service,address,yyzz,headimg,description,quality,service,tag")
-                        ->where(['id'=>$shop_id])
-                        ->find();
-        $shopInfo['shop_fans']=ShopModel::get($shop_id)->shopFans()->count();
-        $shopInfo['shop_goods_num']=ShopModel::get($shop_id)->shopGoodsNum()->count();
-        $shopInfo['shoplogo'] = $this->domain().$shopInfo['shoplogo'];       
-        $shopInfo['sale_num']=ShopModel::shopOrderNum($shop_id);
-        $shopInfo['shop_address']=$shopInfo['province'].$shopInfo['city'].$shopInfo['street'].$shopInfo['address'];
-        if($shopInfo['yyzz']){
-            $yyzz=explode(',',$shopInfo['yyzz']);
-            for($i=0; $i<count($yyzz); $i++){
-                $yyzz[$i]=$this->domain().$yyzz[$i];
+    public function shopinfos()
+    {
+        $shop_id = input("post.shop_id");
+        $shopInfo = Db::name("shop")
+            ->field("shoplogo,name,intro,content,longitude,latitude,addtime,star,province,city,area,street,quality,service,address,yyzz,headimg,description,quality,service,tag")
+            ->where(['id' => $shop_id])
+            ->find();
+        $shopInfo['shop_fans'] = ShopModel::get($shop_id)->shopFans()->count();
+        $shopInfo['shop_goods_num'] = ShopModel::get($shop_id)->shopGoodsNum()->count();
+        $shopInfo['shoplogo'] = $this->domain() . $shopInfo['shoplogo'];
+        $shopInfo['sale_num'] = ShopModel::shopOrderNum($shop_id);
+        $shopInfo['shop_address'] = $shopInfo['province'] . $shopInfo['city'] . $shopInfo['street'] . $shopInfo['address'];
+        if ($shopInfo['yyzz']) {
+            $yyzz = explode(',', $shopInfo['yyzz']);
+            for ($i = 0; $i < count($yyzz); $i++) {
+                $yyzz[$i] = $this->domain() . $yyzz[$i];
             }
-            $shopInfo['yyzz']=$yyzz;
+            $shopInfo['yyzz'] = $yyzz;
         }
-          $shopInfo['tag']=json_decode($shopInfo['tag'],true);
-           
-        // $count=Db::name('shop')->count();
-        // $shops=Db::name('shop')->field("SUM(description) description,SUM(quality) quality,SUM(service) service")->find();
-        // $description=$shops['description']/$count;//平均描述
-        // $quality=$shops['quality']/$count;//平均质量
-        // $service=$shops['service']/$count;//平均服务
-        // if($description==0){
-        //     $shopinfo['description']=array(
-        //         'description'=>0,
-        //         'rate'=>0
-        //     );
-        // }else{
-        //     $shopInfo['description']=array(
-        //         'description'=>$shopInfo['description'],
-        //         'rate'=>round((($shopInfo['description']-$description)/$description*100),2)."%",
-        //     );
-        // }       
-        
-       
-        // if($quality==0){
-        //     $shopInfo['quality']=array(
-        //         'quality'=>0,
-        //         'rate'=>0
-        //     );
-        // }else{
-        //     $shopInfo['quality']=array(
-        //         'quality'=>$shopInfo['quality'],
-        //         'rate'=>round((($shopInfo['quality']-$quality)/$quality*100),2)."%",
-        //     );
-        // }
-        // if($service==0){
-        //     $shopInfo['quality']=array(
-        //         'quality'=>0,
-        //         'rate'=>0
-        //     );
-        // }else{
-        //     $shopInfo['service']=array(
-        //         'service'=>$shopInfo['service'],
-        //         'rate'=>round((($shopInfo['service']-$service)/$service*100),2)."%",
-        //     );
-        // }
-        
-       $this->json_success($shopInfo,'获取数据成功！');
-        
+        $shopInfo['tag'] = json_decode($shopInfo['tag'], true);
+        $this->json_success($shopInfo, '获取数据成功！');
+        die;
     }
-    
-    
-
 }
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,67 @@
 <?php
+
 namespace app\index\controller;
+
+use think\Request;
 
 class Index
 {
     public function index()
     {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架1</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
+    }
+
+
+
+    //注册申请
+    public function apply()
+    {
+        $appid = "wx4d7fa47003d86b9e";
+        $appsecret="3721c1001b3163f514fdcbe6fa748864";
+        if(input('code')){
+            $code=input('code');
+            $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$appsecret."&code=".$code."&grant_type=authorization_code";
+            $data=$this->httpUtil($oauth2Url);                
+            $getInfo=json_decode($data,true);                     
+            $access_token = $getInfo['access_token'];
+            $openid = $getInfo['openid'];  
+            var_dump($getInfo);
+            $get_user_info_url = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid&lang=zh_CN";
+            $data=$this->httpUtil($get_user_info_url);
+            var_dump($data);
+            exit;
+        }else{
+            $redirect_uri = urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+            $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" . $appid . "&redirect_uri=" . $redirect_uri . "&response_type=code&scope=snsapi_userinfo&state=STATE&connect_redirect=1#wechat_redirect";
+            header("Location:" . $url);
+        }
+        
+        exit;
+       
+    }
+    public function httpUtil($url, $data = '', $method = 'GET')
+    {
+        try {
+
+            $curl = curl_init(); // 启动一个CURL会话
+            curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 对认证证书来源的检查
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); // 从证书中检查SSL加密算法是否存在
+            curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+            curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+            if ($method == 'POST') {
+                curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+                if ($data != '') {
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data); // Post提交的数据包
+                }
+            }
+            curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+            curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+            $tmpInfo = curl_exec($curl); // 执行操作
+            curl_close($curl); // 关闭CURL会话
+            return $tmpInfo; // 返回数据
+        } catch (Exception $e) { }
     }
 }
